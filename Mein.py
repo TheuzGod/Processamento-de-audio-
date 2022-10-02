1c836dd64aad57c75bd8b76915cd56e7c0adf1ca
Code Open <101230469+TheuzGod@users.noreply.github.com>
2022-10-02 01:40:36 -0300

    Update Mein.py

--- Mein.py
+++ Mein.py
@@ -8,3 +8,14 @@ urllib.request.urlretrive("https://tinyurl.com/wx9amev", "matallic-dums.wav")
 loop = AudioSegment.from_wav("metallic-drum.wav")
 #Jogue o resultado
 play(loop)
+
+#PROCESSAMENTO BÁSICO DE ÁUDIO
+
+#Repita 2 vezes
+loop2 = loop * 2:
+#Obter comprimento em milissegundos
+length = Len(loop2)
+#Definir o tempo de desvanecimento
+fade_time = int(length * 0.5")
+#Fade dentro e fora
+fade = loop2.falle_in(fade_time).fade_out(fade_time)
